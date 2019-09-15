2e57fd3b9d912743994e17ec1cff05b21fe6659b
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-09-15 18:25:24 +1000

    refactored web3.js code in order to manage the case users are not runnning metamask on their browser

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,13 +1,34 @@
 import Web3 from 'web3';
+require('dotenv').config({path: '../.env'})
 
 // this is trying to access to Metamask
-const web3 = new Web3(window.web3.currentProvider);
+
 
 // const getProvider = async () => {
 //   await window.web3.currentProvider.enable();
 // }
 // getProvider();
 
+
+// 'window' variable is usually defined on browser side
+// Next.js uses a server side rendering system(SSR)
+// therefor 'window' variable should be defined on
+// the server side.
+// const web3 = new Web3(window.web3.currentProvider);
 // const web3 = new Web3(window.ethereum);
+
+let web3;
+
+// if see 
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // we are in the browser and metamask is running.
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // we are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/v3/' + process.env.RINKEBY_API
+  );
+  web3 = new Web3(provider)
+}
 
 export default web3;
